434f2f4102686fa59743e537dd55f4dfe9e66c55
Oluwateniola Adegbulugbe <tenitemi@yahoo.co.uk>
2022-07-07 18:48:44 +0100

    dev branch

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=msudagramdbdev
+export POSTGRES_PASSWORD=msudagramdbdev
+export POSTGRES_HOST=msudagramdb-dev.c66fnwv44hng.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=msudagramdbdev
+export AWS_BUCKET=arn:aws:s3:::microsudagram-dev
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
